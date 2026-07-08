ea728c5030f66352544a5ec953fa97a8131fe076
Mouli1912 <19moulidhara@gmail.com>
2026-07-08 22:37:07 +0530

    feat: refactor application structure and remove Antigravity component
    - Removed Antigravity component and its associated styles.
    - Added new Hero component to replace the visual section.
    - Updated App component to use the new Hero component.
    - Deleted unused assets (hero.png, react.svg, vite.svg).
    - Created new components: About, Contact, Footer, Navber, Projects, Skills.
    - Updated vite.config.js to include .glb assets.
    - Added new dependencies for @react-three/drei, @react-three/rapier, and meshline.

---
+++ src/componenets/Hero.jsx
@@ -0,0 +1,11 @@
+ function Hero (){
+    return(
+        <section>
+            <h1>Hello there </h1>
+            <h1>I'm Mouli  </h1>
+            <p>Frontend Developer </p>
+        </section>
+    );
+ }
+ export default  Hero;
+ 
